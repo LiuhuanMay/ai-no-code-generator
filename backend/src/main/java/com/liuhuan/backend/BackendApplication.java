c98bcb5daa404b7158435b874599723eb047b58a
刘欢 <2983210945@qq.com>
2026-01-19 14:45:50 +0800

    用户模块后端bug修复 启动类上添加MapperScan主机扫描mapper层

--- backend/src/main/java/com/liuhuan/backend/BackendApplication.java
+++ backend/src/main/java/com/liuhuan/backend/BackendApplication.java
@@ -1,10 +1,12 @@
 package com.liuhuan.backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.liuhuan.backend.mapper")
 public class BackendApplication {
 
     public static void main(String[] args) {
