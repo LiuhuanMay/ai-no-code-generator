536c1c383613cf7aa3d91a00302805878e77ec4b
刘欢 <2983210945@qq.com>
2026-01-21 13:00:25 +0800

    补交AppMapper

---
+++ backend/src/main/java/com/liuhuan/backend/mapper/AppMapper.java
@@ -0,0 +1,14 @@
+package com.liuhuan.backend.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.liuhuan.backend.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author L_H
+ * @since 2026-01-20 17:01:29
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
