54d1204a29ce5eb163479eae6bb6ef016b0b3a1f
刘欢 <2983210945@qq.com>
2026-01-23 10:59:43 +0800

    功能扩展 开发截图服务

---
+++ backend/src/main/java/com/liuhuan/backend/service/ScreenshotService.java
@@ -0,0 +1,10 @@
+package com.liuhuan.backend.service;
+
+/**
+ * @author L_H
+ * @since 2026-01-23 10:37:56
+ */
+public interface ScreenshotService {
+
+    String generateAndUploadScreenshot(String webUrl);
+}
